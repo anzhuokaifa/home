aa28ee0265cd152a379c7e588d49740fd27feac3
zhangxiaolong <997178426@qq.com>
2017-08-30 20:07:09 +0800

    First CustomView

---
+++ app/src/main/java/app/android/com/myapp/MyTextView.java
@@ -0,0 +1,48 @@
+package app.android.com.myapp;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/8/30 0030.
+ */
+
+public class MyTextView extends View {
+    private String text;
+    private int textColor;
+    private float textSize;
+    private TypedArray typedArray;
+
+    public MyTextView(Context context) {
+        super(context);
+        initAttr(null);
+    }
+
+
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initAttr(attrs);
+    }
+    private void initAttr(@Nullable AttributeSet attrs) {
+        typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.MyTextView);
+        text = typedArray.getString(R.styleable.MyTextView_text);
+        textSize = typedArray.getDimension(R.styleable.MyTextView_textSize,16);
+        textColor = typedArray.getColor(R.styleable.MyTextView_textColor, 0xffffff);
+    }
+    //绘画
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setTextSize(textSize);
+        paint.setColor(textColor);
+        canvas.drawText(text,10,100,paint);
+    }
+
+}
